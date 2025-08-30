ef0d24c3d436e70958efb297bc58ad4115e97549
Rudra Tyagi <144904870+Rudratyagi1@users.noreply.github.com>
2025-08-30 16:51:29 +0530

    Update vectorstore.py

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -3,10 +3,11 @@ from langchain_community.vectorstores import Chroma
 from langchain.schema import Document
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-def build_vectorstore(chunks: list[Document]):
+def build_vectorstore(chunks: list[Document], persist_dir: str="./chroma_db"):
     vect = Chroma.from_documents(
         documents=chunks,
-        embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
+        persist_directory=persist_dir
     )
-
+    vect.persist()
     return vect.as_retriever(search_type="similarity", search_kwargs={"k":5})
